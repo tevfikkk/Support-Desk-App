1002397fc7a0e1d1a22fffe0d19c024250ae704d
[Tevfik] <tevfikhoglu@gmail.com>
2022-04-07 16:32:50 +0300

    ticket1

---
+++ frontend/src/features/tickets/ticketSlice.js
@@ -0,0 +1,50 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+import ticketService from './ticketService'
+
+const initialState = {
+  tickets: [],
+  ticket: {},
+  isError: false,
+  isSuccess: false,
+  isLoading: false,
+  message: '',
+}
+
+// Create new ticket
+export const createTicket = createAsyncThunk(
+  'tickets/create',
+  async (ticketData, thunkAPI) => {
+    try {
+      const token = thunkAPI.getState().auth.user.token // fetches token
+      return await ticketService.createTicket(ticketData, token)
+    } catch (error) {
+      const message = error.response?.data?.message || error.message?.toString()
+
+      return thunkAPI.rejectWithValue(message)
+    }
+  }
+)
+
+export const ticketSlice = createSlice({
+  name: 'ticket',
+  initialState,
+  reducers: {
+    reset: state => initialState,
+  },
+  extraReducers: builder => {
+    builder
+      .addCase(createTicket.pending, state => (state.isLoading = true))
+      .addCase(createTicket.fulfilled, state => {
+        state.isLoading = false
+        state.isSuccess = true
+      })
+      .addCase(createTicket.rejected, (state, action) => {
+        state.isLoading = false
+        state.isError = true
+        state.message = action.payload
+      })
+  },
+})
+
+export const { reset } = ticketSlice.actions
+export default ticketSlice.reducer
--- frontend/src/pages/NewTicket.jsx
+++ frontend/src/pages/NewTicket.jsx
@@ -1,10 +1,96 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+import { toast } from 'react-toastify'
+import { createTicket, reset } from '../features/tickets/ticketSlice'
+import Spinner from '../components/Spinner'
 
 const NewTicket = () => {
+  const { user } = useSelector(state => state.auth)
+  const { isLoading, isError, isSuccess, message } = useSelector(
+    state => state.ticket
+  )
+
+  const [name] = useState(user.name)
+  const [email] = useState(user.email)
+  const [product, setProduct] = useState('')
+  const [description, setDescription] = useState('')
+
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    if (isError) {
+      toast.error(message)
+    }
+
+    if (isSuccess) {
+      dispatch(reset())
+      navigate('/tickets')
+    }
+
+    dispatch(reset())
+  }, [dispatch, isError, isSuccess, navigate, message])
+
+  const onSubmit = e => {
+    e.preventDefault()
+    dispatch(createTicket({ product, description }))
+  }
+
+  if (isLoading) {
+    return <Spinner />
+  }
+
   return (
-    <div>
-      <h1>New Ticket</h1>
-    </div>
+    <>
+      <section className='heading'>
+        <h1>Create New Ticket</h1>
+        <p>Please fill out the for below</p>
+      </section>
+
+      <section className='form'>
+        <div className='form-group'>
+          <label htmlFor='name'>Customer Name</label>
+          <input type='text' value={name} className='form-control' disabled />
+        </div>
+
+        <div className='form-group'>
+          <label htmlFor='name'>Customer Email</label>
+          <input type='text' value={email} className='form-control' disabled />
+        </div>
+
+        <form onSubmit={onSubmit}>
+          <div className='form-group'>
+            <label htmlFor='product'>Product</label>
+            <select
+              name='product'
+              id='product'
+              value={product}
+              onChange={e => setProduct(e.target.value)}>
+              <option value='Xbox 360'>Xbox 360</option>
+              <option value='Nintendo Game Boy'>Nintendo Game Boy</option>
+              <option value='PS3'>PS3</option>
+              <option value='Sega Genesis'>Sega Genesis</option>
+            </select>
+          </div>
+
+          <div className='form-group'>
+            <label htmlFor='description'>Description of the issue</label>
+            <textarea
+              name='description'
+              id='description'
+              className='form-control'
+              placeholder='Description'
+              value={description}
+              onChange={e => setDescription(e.target.value)}></textarea>
+          </div>
+
+          <div className='form-group'>
+            <button className='btn btn-block'>Submit</button>
+          </div>
+        </form>
+      </section>
+    </>
   )
 }
 
